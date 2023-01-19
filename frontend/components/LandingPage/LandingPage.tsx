54aa7a92b7d5a99442b0bb61807faafc8397032a
Vaibhav Arora <aroravaibhav817@gmail.com>
2023-01-20 00:36:18 +0530

    Fixed some UI issues

--- frontend/components/LandingPage/LandingPage.tsx
+++ frontend/components/LandingPage/LandingPage.tsx
@@ -1,26 +1,39 @@
 import { useRouter } from "next/router";
-
+import { Skeleton } from "@mui/material";
+import Image from "next/image";
 import classes from "./LandingPage.module.css";
 
-const LandingPage = ():JSX.Element => {
-    const router = useRouter();
+const LandingPage = (): JSX.Element => {
+  const router = useRouter();
 
-    const routeHandler = () => {
-        router.push("/dashboard");
-    }
+  const routeHandler = () => {
+    router.push("/dashboard");
+  };
 
-    return (
-        <div className={classes.landingPage}>
-            <div className={classes.details}>
-                <h1>Easily manage your <br /><span>Superfluid</span> streams</h1>
-                <h4>Easily Control all of your superfluid streams in one place <br /> without any technincal knowledge and experience <br /> the power of web3.</h4>
-                <button className="btn btn-warning btn-wide" onClick={routeHandler}>Explore</button>
-            </div>
-            <div className={classes.video}>
-                <video  src="./superfluid.mp4" loop muted playsInline autoPlay/>
-            </div>
+  return (
+    <>
+      <div className={classes.landingPage}>
+        <div className={classes.details}>
+          <h1>
+            Easily manage your <br />
+            <span>Superfluid</span> streams
+          </h1>
+          <h4>
+            Easily Control all of your streams in one place <br /> and unlock
+            the full potential of superfluid <br /> with simplefluid.
+          </h4>
+          <button className="btn btn-warning btn-wide" onClick={routeHandler}>
+            Explore
+          </button>
         </div>
-    )
+        <div className={classes.video}>
+          <video src="./superfluid.mp4" loop muted playsInline autoPlay />
+        </div>
+      </div>
+      <div className={classes.description}>
+      </div>
+    </>
+  );
 };
 
 export default LandingPage;
--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html id="html" lang="en" data-theme="emerald">
+    <Html id="html" lang="en" data-theme="bumblebee">
       <Head>
         <link rel={"stylesheet"} href="https://pro.fontawesome.com/releases/v6.0.0-beta1/css/all.css" />
         <link href="https://cdn.lineicons.com/3.0/lineicons.css" rel="stylesheet"></link>
